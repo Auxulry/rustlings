c7fc1f6ffe9a6f2032d1ba1d2cf834479d50ba72
Mochamad Akbar <mochakbar912@gmail.com>
2023-10-29 23:37:06 +0700

    feat(macros): rust exercies macros

--- exercises/macros/macros3.rs
+++ exercises/macros/macros3.rs
@@ -8,6 +8,7 @@
 // I AM NOT DONE
 
 mod macros {
+    #[macro_export]
     macro_rules! my_macro {
         () => {
             println!("Check out my macro!");
@@ -16,5 +17,7 @@ mod macros {
 }
 
 fn main() {
+    use my_macro;
+
     my_macro!();
 }
